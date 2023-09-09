6d1817ff1161000bb5b2015110f13a717089e205
Bijo Thomas <91835086+bjothomas@users.noreply.github.com>
2023-09-09 21:42:36 +0530

    Create Char count.py

---
+++ Char count.py
@@ -0,0 +1,7 @@
+input_string = input("Enter a string: ")
+char_to_count = input("Enter the character to count: ")
+count = 0
+for char in input_string:
+    if char == char_to_count:
+        count += 1
+print(f"The character '{char_to_count}' occurs {count} times in the string.")
